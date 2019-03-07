bd85ba19139e9eaa74b8aedde4c7f7aa86d9f9b4
wiwitauliarahman <48266154+wiwitauliarahman@users.noreply.github.com>
2019-03-07 18:10:46 +0700

    Add files via upload

---
+++ wiwitauliarahman170900803A.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wiwit;
+
+/**
+ *
+ * @author aku
+ */
+public class wiwitauliarahman170900803A {
+    public static void main(String[] args) {
+        System.out.println("wiwit.wiwitauliarahman170900803A.main()");
+        System.out.println("wiwit.wiwitauliarahman170900803A.main()");
+        System.out.println("wiwit.wiwitauliarahman170900803A.main()");
+    }
+    
+}
